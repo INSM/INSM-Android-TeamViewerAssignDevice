12878f18fa571c24fc8806237355caa2d31d827d
Filip <filip.kantar@insm.se>
2019-12-22 12:16:18 +0100

    Initial commit

---
+++ app/src/main/java/com/insm/teamviewerassigndevice/MainActivity.java
@@ -0,0 +1,24 @@
+package com.insm.teamviewerassigndevice;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        launchWebPage(getString(R.string.teamviewer_url));
+    }
+
+    public void launchWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        System.exit(0);
+    }
+}
